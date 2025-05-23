fa593332a73069ac73cbc873cbf52e162111dc94
ShunsukeYokota(横田峻亮) <shunsuke.yokota.b022@dirbato.co.jp>
2025-05-23 11:25:17 +0900

    Gitの練習です

--- Hellow World/src/first/Hello.java
+++ Hellow World/src/first/Hello.java
@@ -3,7 +3,8 @@ package first;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	System.out.println("Hello, World!");
+	System.out.println("こんにちは、世界！");
 
 	}
 
